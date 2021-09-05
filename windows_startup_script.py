73b9c7043ef42d262ab3ff998602c2b70fa52fc0
Zaenumbot <87750993+Zaenbot@users.noreply.github.com>
2021-09-05 16:30:13 +0700

    Update windows_startup_script.py

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
-API_KEY = "Type that here"
-API_HASH = "Type that here"
+API_KEY SINI-TOD = "Type that here"
+API_HASH SINI-TOD = "Type that here"
 # get it from my.telegram.org
 bot = TelegramClient('userbot', API_KEY, API_HASH)
 bot.start()
